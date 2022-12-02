a3deed48c5dd9ef02631a65998ead58318a35ada
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-12-02 05:15:28 +0300

    feat: create user service

--- types.d.ts
+++ types.d.ts
@@ -29,3 +29,17 @@ export type Genre = {
   id: number;
   name: string;
 };
+
+export type User = {
+  avatar: {
+    gravatar: {
+      hash: string;
+    };
+  };
+  id: number;
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  include_adult: boolean;
+  username: string;
+};
